b1bcd7e25942a22f315d563c4220525da347bfba
Bjørn Kristian Punsvik <bk.punsvik@gmail.com>
2018-03-22 17:19:26 +0100

    added som on 5

--- oppgave4_d.py
+++ oppgave4_d.py
@@ -1,21 +1,62 @@
 from oppgave4 import *
 from konstanter import *
+import numpy as np
+from scipy.sparse import lil_matrix
 
 
 def lagVektor(n):
-    konstantledd = f / (E * I)
-    a = [konstantledd for x in range(0, n)]
-    return a
+    b = np.ones(n)
+    b = [f/(E*I) for i in range(n)]
+    return b
+
+
+def finnForoverfeil(x, xa):
+    out = []
+    for i in range(0, len(x)):
+        out.append(abs(x[i]-xa[i]))
+    return out
+
+
+def finnRelativForoverfeil(x, xa):
+    out = []
+    for i in range(0, len(x)):
+        out.append((abs(x[i] - xa[i]))/(abs(x[i])))
+    return out
+
+
+def finnFeilforstoring(RFE, RBE):
+    out = []
+    for i in range(0, len(RFE)):
+        out.append(RFE[i]/RBE)
+    return out
+
+
+def finnKondisjonstallet(A):
+    return abs(A)*abs(np.linalg.inv(A))
 
 
 if __name__ == "__main__":
-    ye = ye()
-    v = lagVektor(10)
-    fjerde = fjerdeDeriverte(10)
-    print('\nFjerdederiverte:')
-    for i in fjerde:
-        print(i)
+    n = 10
+    RBE = 2**(-52)
 
-    for i in range(0, 10):
-        forskjell = ye[i] - f / (E * I)
-        print('i', i, '=', forskjell)
+    vektor = lagVektor(n)
+    fjerde = fjerdeDeriverte(n)
+
+    FE = finnForoverfeil(vektor, fjerde)
+    RFE = finnRelativForoverfeil(vektor, fjerde)
+    EMF = finnFeilforstoring(RFE, RBE)      #Error Magnification Factor
+
+    print(FE)
+    print(RFE)
+    print(EMF)
+
+    A = lagA(n)
+    Akond = finnKondisjonstallet(A)
+
+    print(Akond)
+
+
+
+
+
+
--- oppgave5.py
+++ oppgave5.py
@@ -3,21 +3,21 @@ from oppgave3 import *
 from oppgave4_d import lagVektor
 import numpy as np
 
+
 def finnFeil(n):
-	volum = L * w * d
-	vekt = volum * p
-	b = np.array([vekt for x in range(n)])
-	y = solveAy(n, b)
-	
-	return y[Lint]
-	
-	
+    volum = L * w * d
+    vekt = volum * p
+    b = np.array([vekt for x in range(n)])
+    y = solveAy(n, b)
+
+    return y[Lint]
+
 
 for i in range(1, 12):
-	n = 10*2**i
-	print(n)
-	a = lagVektor(n)
-	feil = finnFeil(n)
-	print(feil)
-	print(feil-a[Lint])
-	print()
+    n = 10 * 2 ** i
+    print(n)
+    a = lagVektor(n)
+    feil = finnFeil(n)
+    print(feil)
+    print(feil - a[Lint])
+    print()
